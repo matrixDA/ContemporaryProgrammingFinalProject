bd6b39ce6e59082fb776043652a75fcf3647ef79
Loc Nguyen <loc.nguyen.leo@gmail.com>
2023-11-28 14:09:59 -0500

    API Controller for Food and Hobbies.

---
+++ ContemporaryProgrammingFinalProject/Controllers/APIController.cs
@@ -0,0 +1,131 @@
+﻿using ContemporaryProgrammingFinalProject.Data;
+using ContemporaryProgrammingFinalProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContemporaryProgrammingFinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        IFinalService ctx;
+        public APIController(IFinalService context)
+        {
+            ctx = context;
+        }
+        //FavoriteFoodAPI
+        [HttpGet]
+        [Route("api/getfood")]
+        public IActionResult GetFood()
+        {
+            return Ok(ctx.GetAllFood());
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetFood(int id)
+        {
+            return Ok(ctx.GetFoodById(id));
+        }
+
+        [HttpPost("api/addfood")]
+        public IActionResult PostFood(FavoriteFood i)
+        {
+            var result = ctx.AddFood(i);
+            if (result == null)
+            {
+                return StatusCode(500, "An item with this ID already exists");
+            }
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpPut("api/updatefood")]
+        public IActionResult PutFood(FavoriteFood i)
+        {
+            var result = ctx.UpdateFood(i);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        [Route("api/deletefood")]
+        public IActionResult DeleteFood(int id)
+        {
+            var product = ctx.GetFoodById(id);
+            if (product == null)
+            {
+                return NotFound(id);
+            }
+            var result = ctx.RemoveFoodById(id);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        //HobbyAPI
+        [HttpGet]
+        [Route("api/gethobby")]
+        public IActionResult GetHobby()
+        {
+            return Ok(ctx.GetAllHobbies());
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetHobby(int id)
+        {
+            return Ok(ctx.GetHobbyById(id));
+        }
+
+        [HttpPost("api/addhobby")]
+        public IActionResult Post(Hobbies i)
+        {
+            var result = ctx.AddHobby(i);
+            if (result == null)
+            {
+                return StatusCode(500, "An item with this ID already exists");
+            }
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpPut("api/updatehobby")]
+        public IActionResult Put(Hobbies i)
+        {
+            var result = ctx.UpdateHobbies(i);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        [Route("api/deletehobby")]
+        public IActionResult Delete(int id)
+        {
+            var product = ctx.GetHobbyById(id);
+            if (product == null)
+            {
+                return NotFound(id);
+            }
+            var result = ctx.RemoveHobbyById(id);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+    }
+}
--- ContemporaryProgrammingFinalProject/Controllers/FFController.cs
+++ ContemporaryProgrammingFinalProject/Controllers/FFController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ContemporaryProgrammingFinalProject.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContemporaryProgrammingFinalProject.Controllers
@@ -7,5 +8,66 @@ namespace ContemporaryProgrammingFinalProject.Controllers
     [ApiController]
     public class FFController : ControllerBase
     {
+        IFinalService ctx;
+        public FFController(IFinalService context)
+        {
+            ctx = context;
+        }
+        [HttpGet]
+        [Route("api/getitems")]
+        public IActionResult Get()
+        {
+            return Ok(ctx.GetItems());
+        }
+
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        {
+            return Ok(ctx.GetItemById(id));
+        }
+
+        [HttpPost("api/additem")]
+        public IActionResult Post(Item i)
+        {
+            var result = ctx.AddItem(i);
+            if (result == null)
+            {
+                return StatusCode(500, "An item with this ID already exists");
+            }
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpPut("api/updateitem")]
+        public IActionResult Put(Item i)
+        {
+            var result = ctx.UpdateItem(i);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        [Route("api/delete")]
+        public IActionResult Delete(int id)
+        {
+            var product = ctx.GetItemById(id);
+            if (product == null)
+            {
+                return NotFound(id);
+            }
+            var result = ctx.RemoveItemById(id);
+            if (result == 0)
+            {
+                return StatusCode(500, "An error occured while processing your request");
+            }
+            return Ok();
+        }
     }
+}
 }
