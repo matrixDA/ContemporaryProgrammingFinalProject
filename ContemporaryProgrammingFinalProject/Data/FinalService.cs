372edfffb43e3dc33391276e5fda35230f36009e
Logan Freeman <loganfree18@gmail.com>
2023-11-28 13:48:47 -0500

    Interface update

--- ContemporaryProgrammingFinalProject/Data/FinalService.cs
+++ ContemporaryProgrammingFinalProject/Data/FinalService.cs
@@ -1,6 +1,115 @@
-﻿namespace ContemporaryProgrammingFinalProject.Data
+﻿using ContemporaryProgrammingFinalProject.Models;
+
+namespace ContemporaryProgrammingFinalProject.Data
 {
-    public class FinalService : IFinalService
-    {
-    }
+	public class FinalService : IFinalService
+	{
+
+		FinalContext FC;
+
+		public FinalService(FinalContext context)
+		{
+			FC = context;
+		}
+
+		public int? AddFood(FavoriteFood i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? AddHobby(Hobbies i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? AddPersonal(PersonalInfo i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? AddVideoGames(VideoGames i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<FavoriteFood> GetAllFood()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Hobbies> GetAllHobbies()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<PersonalInfo> GetAllPersonal()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<VideoGames> GetAllVideoGames()
+		{
+			throw new NotImplementedException();
+		}
+
+		public FavoriteFood GetFoodById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Hobbies GetHobbyById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public PersonalInfo GetPersonalById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public VideoGames GetVideoGamesById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? RemoveFoodById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? RemoveHobbyById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? RemovePersonalById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? RemoveVideoGamesById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? UpdateFood(FavoriteFood i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? UpdateHobbies(Hobbies i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? UpdatePersonal(PersonalInfo i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int? UpdateVideoGames(VideoGames i)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- ContemporaryProgrammingFinalProject/Data/IFinalService.cs
+++ ContemporaryProgrammingFinalProject/Data/IFinalService.cs
@@ -1,7 +1,37 @@
-﻿namespace ContemporaryProgrammingFinalProject.Data
+﻿using ContemporaryProgrammingFinalProject.Models;
+
+namespace ContemporaryProgrammingFinalProject.Data
 {
-    public interface IFinalService
+	public interface IFinalService
     {
 
-    }
+		int? AddHobby(Hobbies i);
+		List<Hobbies> GetAllHobbies();
+		Hobbies GetHobbyById(int Id);
+		int? RemoveHobbyById(int Id);
+		int? UpdateHobbies(Hobbies i);
+
+
+		int? AddFood(FavoriteFood i);
+		List<FavoriteFood> GetAllFood();
+		FavoriteFood GetFoodById(int Id);
+		int? RemoveFoodById(int Id);
+		int? UpdateFood(FavoriteFood i);
+
+
+		int? AddPersonal(PersonalInfo i);
+		List<PersonalInfo> GetAllPersonal();
+		PersonalInfo GetPersonalById(int Id);
+		int? RemovePersonalById(int Id);
+		int? UpdatePersonal(PersonalInfo i);
+
+
+		int? AddVideoGames(VideoGames i);
+		List<VideoGames> GetAllVideoGames();
+		VideoGames GetVideoGamesById(int Id);
+		int? RemoveVideoGamesById(int Id);
+		int? UpdateVideoGames(VideoGames i);
+
+
+	}
 }
