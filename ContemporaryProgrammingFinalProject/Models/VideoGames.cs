b588d0179eb90792a9b41166ae32b578ae9078d0
Ian Mendez <mendezij@mail.uc.edu>
2023-11-28 13:03:03 -0500

    Ian VG Model Create

--- ContemporaryProgrammingFinalProject/Models/VideoGames.cs
+++ ContemporaryProgrammingFinalProject/Models/VideoGames.cs
@@ -4,7 +4,11 @@
 	{
 		public int ID { get; set; }
 
-		public int Member { get; set; }
+		public string? Member { get; set; }
+		public string? Developer { get; set; }
+		public int PlayTime { get; set;}
+		public string? Genre { get; set; }
+		public int Score { get; set; }
 
 	}
 }
